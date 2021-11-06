3800f9fc809d305dda2ee2e2224583f78f0282d0
Roberto de Leon <robedeleong@gmail.com>
2021-11-06 04:59:06 -0600

    added requests and handled states

--- lib/pages/home/widgets/header_content.dart
+++ lib/pages/home/widgets/header_content.dart
@@ -1,79 +1,143 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scheduler_frontend/models/nurse/nurse.dart';
+import 'package:scheduler_frontend/services/nurse_service.dart';
 
 class HeaderContent extends StatefulWidget {
-  const HeaderContent({Key? key}) : super(key: key);
+  const HeaderContent({
+    Key? key,
+    required this.searchKey,
+    this.onKeyChanged,
+    this.onNurseSelected,
+  }) : super(key: key);
+
+  final String searchKey;
+  final Function(String)? onKeyChanged;
+  final Function(Nurse)? onNurseSelected;
 
   @override
   _HeaderContentState createState() => _HeaderContentState();
 }
 
 class _HeaderContentState extends State<HeaderContent> {
+  NurseService nurseService = NurseService();
+
+  Nurse? selectedNurse;
+
   @override
   Widget build(BuildContext context) {
-    Widget _nurseCodeTile() {
-      return const Padding(
-        padding: EdgeInsets.only(left: 50),
-        child: ListTile(
-          leading: Icon(
-            Icons.local_hospital,
-            size: 40,
-            color: Colors.blue,
-          ),
-          title: Text(
-            "1234",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w300,
+    Widget _nurseCodeTile(Nurse nurse) {
+      return Padding(
+        padding: const EdgeInsets.only(left: 25),
+        child: Card(
+          elevation: 2,
+          child: ListTile(
+            onTap: () {
+              setState(() {
+                selectedNurse = nurse;
+                widget.onKeyChanged?.call(selectedNurse!.nurseId!.toString());
+              });
+            },
+            leading: const Icon(
+              Icons.local_hospital,
+              size: 40,
+              color: Colors.blue,
+            ),
+            title: Text(
+              nurse.nurseId.toString(),
+              style: const TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w300,
+              ),
             ),
           ),
         ),
       );
     }
 
-    return Expanded(
-      child: Row(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) => _nurseCodeTile(),
-              itemCount: 20,
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 300),
+      child: Expanded(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: FutureBuilder(
+                future: nurseService.getAllNurses(),
+                builder: (context, AsyncSnapshot<List<Nurse>> snapshot) {
+                  List<Nurse> nurses = [];
+                  Widget? child;
+                  if (snapshot.hasData && snapshot.data?.isNotEmpty == true) {
+                    if (widget.searchKey != '') {
+                      nurses = snapshot.data!
+                          .where((nurse) =>
+                              nurse.nurseId.toString().contains(widget.searchKey))
+                          .toList();
+                    } else {
+                      nurses = snapshot.data!;
+                    }
+
+                    if (nurses.isNotEmpty) {
+                      child = ListView.builder(
+                        itemBuilder: (context, index) =>
+                            _nurseCodeTile(nurses[index]),
+                        itemCount: nurses.length,
+                      );
+                    } else {
+                      child = const Text(
+                          "No hay enfermeras que coincidan con esa clave");
+                    }
+                  } else {
+                    child = const CupertinoActivityIndicator(
+                      radius: 25,
+                    );
+                  }
+
+                  return AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 100),
+                    child: child,
+                  );
+                },
+              ),
             ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Nombre: Fulanita de tal",
-                  style: TextStyle(fontSize: 27),
-                ),
-                const Divider(),
-                const Text(
-                  "Horario: 10 a 20",
-                  style: TextStyle(fontSize: 27),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 40),
-                  child: MaterialButton(
-                    minWidth: 400,
-                    padding: const EdgeInsets.all(20),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    color: Colors.blue,
-                    onPressed: () {
-                      FocusScope.of(context).requestFocus(FocusNode());
-                    },
-                    child: const Text(
-                      "Ingresar",
-                      style: TextStyle(fontSize: 20, color: Colors.white),
+            if (selectedNurse != null)
+              Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      selectedNurse!.name!,
+                      style: const TextStyle(fontSize: 27),
                     ),
-                  ),
+                    const Divider(),
+                    const Text(
+                      "Horario: 10 a 20",
+                      style: TextStyle(fontSize: 27),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: MaterialButton(
+                        minWidth: 400,
+                        padding: const EdgeInsets.all(20),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        color: Colors.blue,
+                        onPressed: () {
+                          widget.onNurseSelected?.call(selectedNurse!);
+                          FocusScope.of(context).requestFocus(FocusNode());
+                        },
+                        child: const Text(
+                          "Ingresar",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
-        ],
+              ),
+          ],
+        ),
       ),
     );
   }
